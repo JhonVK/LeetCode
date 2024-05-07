77a02be3209bc4807fb7f0aac4d84fbf0f87d744
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-05-07 20:29:30 -0300

    1

---
+++ SearchInsertPosition.c
@@ -0,0 +1,13 @@
+//mt facil
+int searchInsert(int* nums, int numsSize, int target) {
+int posicReal=0;
+for(int i=0; i<numsSize;i++){
+    if(nums[i]==target){
+        return i;
+    }
+    if(nums[i]<target){
+        posicReal++;
+    }
+}
+return posicReal;
+}
---
+++ addTwoNumbers.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    int sum = 0;
+    struct ListNode *head = NULL, *tail = NULL;
+    while (l1 || l2 || sum) {
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        struct ListNode *node = malloc(sizeof(struct ListNode));
+        node->val = sum % 10;
+        node->next = NULL;
+        sum /= 10;
+        if (!head) {
+            head = node;
+        } else {
+            tail->next = node;
+        }
+        tail = node;
+    }
+    return head;
+}

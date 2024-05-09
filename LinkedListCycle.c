0830960db4c8d487b0d953f26801f42f2fcb5ebe
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-05-09 11:46:59 -0300

    update

---
+++ LinkedListCycle.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+ //Using Tortoise and Hare Algorithm
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *fast = head, *slow = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}

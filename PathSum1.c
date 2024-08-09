23e8522d81a3fb44e0576eea5679f5b4a0dc0c12
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-08-08 23:16:19 -0300

    update

--- PathSum1.c
+++ PathSum1.c
@@ -8,12 +8,12 @@
  */
 
 bool hasPathSum(struct TreeNode* root, int targetSum) {
-   if(root==NULL){return false;}
+    if(root==NULL){return false;}
 
-   targetSum-=root->val;
+    targetSum-=root->val;
 
-   if(root->left==NULL && root->right==NULL){
-    return targetSum==0;
-   }
+    if(root->left==NULL && root->right==NULL){
+        return targetSum==0;
+    }
     return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
 }
